81ef63a61e8c28ea4aaa36f6f86983b59ad6e05b
amrit singh bains <amrit.singh.bains@live.com>
2016-04-13 20:18:54 -0400

    get count

--- create_table.sql
+++ create_table.sql
@@ -8,3 +8,7 @@ option4 TEXT,
 answer	TEXT
 );
 
+insert into superhero (question, option1, option2, option3, option4, answer) VALUES 
+	('Who lives in Gotham City', 'batman', 'superman', 'wonderwoman', 'aliens');
+
+
